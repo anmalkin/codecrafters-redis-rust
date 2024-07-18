291b164d4cbd7c2d8c1aef71a3c879b45d55b4d7
amalkin <102127060+anmalkin@users.noreply.github.com>
2024-07-17 21:59:35 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use std::net::TcpListener;
+use std::{io::{Read, Write}, net::TcpListener};
 
-fn main() {
+fn main() -> std::io::Result<()> {
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379")?;
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let mut buf = [0; 1024];
+                let n = stream.read(&mut buf)?;
+                println!("received {} bytes", n);
+                let buf: &[u8] = b"+PONG\r\n";
+                stream.write_all(buf)?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    Ok(())
 }
